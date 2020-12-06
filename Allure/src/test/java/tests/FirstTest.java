60c49f6dbe61692159f4381f52ce942fffb588db
birendra <birendrachandd@gmail.com>
2020-12-05 19:02:27 -0500

    Initial Commit

---
+++ Allure/src/test/java/tests/FirstTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+@Test
+
+public void helloWorld()
+{
+	System.out.println("Hello World");
+}
+
+
+}
